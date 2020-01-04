bab939bd868b8a63899300e8e166b1655a2dc881
CaelumTerrae <gavinlifrieri@gmail.com>
2020-01-03 19:36:52 -0500

    updates for deployment to app.py and gitignore

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+application = Flask(__name__)
 
 # Ensure responses aren't cached
-@app.after_request
+@application.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return render_template("index.html")
 
-@app.route('/day/<int:num>')
+@application.route('/day/<int:num>')
 def project(num):
   return render_template("/days/1.html")
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
