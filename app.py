eafe3a693b4c4c3b383d1c31e80efe0330b45c1c
CaelumTerrae <gavinlifrieri@gmail.com>
2020-01-05 19:43:56 -0500

    updated site to handle email

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, request
+from secrets import emailsecret
+from flask_mail import Message, Mail
+
+mail = Mail()
+
 application = Flask(__name__)
+
+application.config["MAIL_SERVER"] = "smtp.gmail.com"
+application.config["MAIL_PORT"] = 465
+application.config["MAIL_USE_SSL"] = True
+application.config["MAIL_USERNAME"] = emailsecret["username"]
+application.config["MAIL_PASSWORD"] = emailsecret["password"]
+
+mail.init_app(application)
+
 numPages = 3 #update this daily, and change this to be a request to database when that is set up
 
 # Ensure responses aren't cached
@@ -15,13 +29,21 @@ def hello_world():
   if request.method =='GET':
     return render_template("index.html")
   else:
-    user = {
+    contact = {
       "name": request.form["name"],
       "email": request.form["email"],
       "subject": request.form["subject"],
       "message": request.form["message"]
     }
-    if user["name"] and user["email"] and user["subject"] and user["message"]:
+    if contact["name"] and contact["email"] and contact["subject"] and contact["message"]:
+      subject = "New Project Every Day: " + contact["subject"]
+      msg = Message(subject, sender=emailsecret["username"], recipients=[emailsecret["username"]])
+      msg.body = """
+      Message from New Project Every Day!
+      From: %s <%s>
+      %s
+      """ % (contact["name"], contact["email"], contact["message"])
+      mail.send(msg)
       return render_template("index.html",success=True,returnAddress="contact")
     else:
       messages = ["Missing fields!"]
